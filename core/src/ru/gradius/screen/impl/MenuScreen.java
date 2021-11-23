40a2008901adebbf1ef92d62669721b1646f2a34
Ivan <reinkarnaci9@gmail.com>
2021-11-23 22:50:14 +0300

    Отрефакторин код под матрицы

--- core/src/ru/gradius/screen/impl/MenuScreen.java
+++ core/src/ru/gradius/screen/impl/MenuScreen.java
@@ -1,50 +1,39 @@
 package ru.gradius.screen.impl;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
+import ru.gradius.math.Rect;
 import ru.gradius.screen.BaseScreen;
+import ru.gradius.sprite.Impl.Background;
 
 public class MenuScreen extends BaseScreen {
 
-    private Texture spaceShip;
+
+    private Texture ship;
     private Texture space;
-    private Vector2 touch;
-    private Vector2 startPoint;
 
-    private Vector2 positiveVector;
-    private Vector2 negativeVector;
+    private Vector2 position;
+    private Background background;
 
-    private Vector2 positiveXvector;
-    private Vector2 negativeXvector;
 
-    private Vector2 positiveYvector;
-    private Vector2 negativeYvector;
 
-    private Vector2 positiveXnegativeYvector;
-    private Vector2 positiveYnegativeXvector;
 
     @Override
     public void show() {
 
         super.show();
-        space = new Texture("warp.png");
-        spaceShip = new Texture("thunderhawk-gunship.png");
-        touch = new Vector2();
-        startPoint = new Vector2(0,0);
+        space = new Texture("textures/warp.png");
+        ship = new Texture("textures/thunderhawk-gunship.png");
+        background = new Background(space);
+        position = new Vector2();
 
-        positiveVector = new Vector2(1,1);
-        negativeVector = new Vector2(-1,-1);
+    }
 
-        positiveXvector = new Vector2(1,0);
-        negativeXvector = new Vector2(-1,0);
-
-        positiveYvector = new Vector2(0,1);
-        negativeYvector = new Vector2(0,-1);
-
-        positiveXnegativeYvector = new Vector2(1,-1);
-        positiveYnegativeXvector = new Vector2(-1,1);
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
     }
 
     @Override
@@ -52,50 +41,22 @@ public class MenuScreen extends BaseScreen {
         super.render(delta);
 
         batch.begin();
-        batch.draw(space, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        batch.draw(spaceShip, startPoint.x, startPoint.y);
+        background.draw(batch);
         batch.end();
 
-        if(startPoint.x != touch.x && startPoint.y != touch.y){
-
-                if (startPoint.x < touch.x && startPoint.y < touch.y) {
-                    startPoint.add(positiveVector);
-                } else if (startPoint.x > touch.x && startPoint.y < touch.y){
-                    startPoint.add(positiveYnegativeXvector);
-                } else if (startPoint.x < touch.x && startPoint.y > touch.y) {
-                    startPoint.add(positiveXnegativeYvector);
-                } else {
-                    startPoint.add(negativeVector);
-                }
-
-        } else if (startPoint.x != touch.x && startPoint.y == touch.y) {
-
-            if (startPoint.x < touch.x) {
-                startPoint.add(positiveXvector);
-            } else {
-                startPoint.add(negativeXvector);
-            }
-
-        } else if (startPoint.x == touch.x && startPoint.y != touch.y) {
-
-            if (startPoint.y < touch.y) {
-                startPoint.add(positiveYvector);
-            } else {
-                startPoint.add(negativeYvector);
-            }
-        }
     }
 
     @Override
     public void dispose() {
         super.dispose();
-        spaceShip.dispose();
+        ship.dispose();
+        space.dispose();
     }
 
     @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
 
-        touch.set(screenX, Gdx.graphics.getHeight() - screenY);
-        return super.touchDown(screenX, screenY, pointer, button);
+        position.set(touch);
+        return super.touchDown(touch, pointer, button);
     }
 }
---
+++ core/src/ru/gradius/sprite/Impl/Background.java
@@ -0,0 +1,20 @@
+package ru.gradius.sprite.Impl;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+import ru.gradius.math.Rect;
+import ru.gradius.sprite.Sprite;
+
+public class Background extends Sprite {
+
+    public Background(Texture texture) {
+        super(new TextureRegion(texture));
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        setHeightProportion(worldBounds.getHeight());
+        this.pos.set(worldBounds.pos);
+    }
+}
