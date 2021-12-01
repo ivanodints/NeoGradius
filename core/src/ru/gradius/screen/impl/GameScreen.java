45b756fd25157a2ff919bf6e942ad0a1e476ae58
Ivan <reinkarnaci9@gmail.com>
2021-12-01 19:36:06 +0300

    Добавлены звук и авто фаер

--- core/src/ru/gradius/screen/impl/GameScreen.java
+++ core/src/ru/gradius/screen/impl/GameScreen.java
@@ -1,5 +1,8 @@
 package ru.gradius.screen.impl;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
@@ -24,9 +27,16 @@ public class GameScreen extends BaseScreen {
     private Star[] stars;
     private MainShip mainShip;
 
+    private Music music;
+    private Sound sound;
+
     @Override
     public void show() {
         super.show();
+
+        sound = Gdx.audio.newSound(Gdx.files.internal("sounds/laser.wav"));
+
+
         bg = new Texture("textures/warp.png");
         background = new Background(bg);
         atlas = new TextureAtlas("textures/mainAtlas.tpack");
@@ -37,7 +47,11 @@ public class GameScreen extends BaseScreen {
         for (int i = 0; i < stars.length; i++) {
             stars[i] = new Star(atlas);
         }
-        mainShip = new MainShip(atlas, bulletPool);
+        mainShip = new MainShip(atlas, bulletPool, sound);
+
+        music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
+        music.setLooping(true);
+        music.play();
     }
 
     @Override
@@ -64,6 +78,8 @@ public class GameScreen extends BaseScreen {
         bg.dispose();
         atlas.dispose();
         bulletPool.dispose();
+        music.dispose();
+        sound.dispose();
     }
 
     @Override
